38daffc899960f5830d781cbb9765e66e3371b9d
Johan george <johangeorge201203@gmail.com>
2023-11-18 22:05:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 const App = () => (
-  <main className="relative">nav
-  <section>
-    hero</section></main>
+  <main className="relative">
+     nav
+  <section className="xl:padding-1 wide:padding-r padding-b">
+    hero
+    </section>
+    <section className="padding"> Popular product</section>
+    <section className="padding"> Popular product</section>
+    <section className="padding"> Popular product</section>
+    <section className="padding"> Popular product</section></main>
 );
 export default App;
